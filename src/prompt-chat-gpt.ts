3357128fb69ea85107e03d1e32615ff29c70e939
william <grisa.william@gmail.com>
2023-08-21 22:55:39 -0300

    Improve gpt

--- src/prompt-chat-gpt.ts
+++ src/prompt-chat-gpt.ts
@@ -11,7 +11,8 @@ const getContentPrompt = ({ type, id, name }: StoryEntity) =>
   and the template for a new feature branch name: "type/id/name-following-rules", e.g.: "feature/123/branch-name", 
   create a branch name not using any special characters from the name. Consider the entire name as the context.
   Return the branch name in lowercase. It needs to be a valid git branch name.
-  Separate all words with a dash, respecting the slashes from the template. Just answer with the branch name.`
+  Separate all words with a dash, respecting the slashes from the template. Just answer with the branch name.
+  Make it below 50 chars.`
 
 export const generateNameUsingChatGPT = async (story: StoryEntity): Promise<string | null> => {
   const content = getContentPrompt(story)
