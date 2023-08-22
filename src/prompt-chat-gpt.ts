ce5a6db81853488c04caf6f1b69dd708f746c516
william <grisa.william@gmail.com>
2023-08-21 22:37:15 -0300

    Simplify params for prompting

--- src/prompt-chat-gpt.ts
+++ src/prompt-chat-gpt.ts
@@ -9,16 +9,12 @@ const openai = new OpenAI({
 const getContentPrompt = ({ type, id, name }: StoryEntity) =>
   `Given the id number ${id}, the story name: ${name}, the story type: ${type}, 
   and the template for a new feature branch name: "type/id/name-following-rules", e.g.: "feature/123/branch-name", 
-  create a branch name not using any special characters from the name, but considering the entire name as the context.
+  create a branch name not using any special characters from the name. Consider the entire name as the context.
   Return the branch name in lowercase. It needs to be a valid git branch name.
   Separate all words with a dash, respecting the slashes from the template. Just answer with the branch name.`
 
-export const generateNameUsingChatGPT = async ({
-  type: storyType,
-  id: storyNumber,
-  name: storyName,
-}: StoryEntity): Promise<string | null> => {
-  const content = getContentPrompt({ type: storyType, id: storyNumber, name: storyName })
+export const generateNameUsingChatGPT = async (story: StoryEntity): Promise<string | null> => {
+  const content = getContentPrompt(story)
 
   const completion = await openai.chat.completions.create({
     messages: [{ role: extensionConfig.openAI.role, content }],
